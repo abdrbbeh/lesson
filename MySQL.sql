28b2de34fa41174e613965398ad37836e18e4c75
abdulrhman <131538237+abdrbbeh@users.noreply.github.com>
2023-08-21 18:52:16 +0300

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,16 +1,35 @@
 # 02
-mysql -u root -p
-CREATE DATABASE `Database_Name`;
-SHOW DATABASES;
-DROP DATABASE `Database_Name`;
 
+mysql -u root -p
 # 03
-CREATE DATABASE IF NOT EXISTS `Database_Name`;
-DROP DATABASE IF EXISTS `Database_Name`;
 SELECT `Column_Name`,... FROM `Table_Name`,... WHERE `Column_Name` = Value AND `Column_Name` = Value ORDER BY `Column_Name`,... LIMIT number;
 SELECT * FROM `Table_Name`;
-INSERT INTO `Database_Name`.`Table_Name` (`Column_Name`,...) VALUES (VALUE,...);
-USE `Database_Name`;
+INSERT INTO `Database_Name`.`Table_Name` (`Column_Name`,...) VALUES (Value,...);
 
 # 05
 TRUNCATE `Table_Name`;
+
+# 07
+ENUM(Value,...)
+UPDATE `Database_Name`.`Table_Name` SET `Column_Name`,... = Value,... WHERE *Coundtion*;
+SET(Value,...)
+INSERT INTO `Database_Name`.`Table_Name` (`Column_Name`,...) VALUES ('Value,...');
+
+# 08
+SHOW DATABASES;
+USE `Database_Name`;
+DROP DATABASE `Database_Name`;
+DROP DATABASE IF EXISTS `Database_Name`;
+CREATE DATABASE IF NOT EXISTS `Database_Name`;
+SHOW DATABASES LIKE 'Database_Name';
+
+# 09
+CREATE TABLE `Table_Name` ( `Column_Name` DataType(Len),...);
+DESCRIBE `Table_Name`;
+SHOW COLUMNS FROM `Table_Name`;
+SHOW FIELDS FROM `Table_Name`;
+SHOW TABLE STATUS;
+SHOW CREATE TABLE `Table_Name`;
+DROP TABLE `Table_Name`;
+DROP TABLE IF EXISTS `Table_Name`;
+CREATE TABLE IF NOT EXISTS `Table_Name`; ( `Column_Name` DataType(Len),...);
